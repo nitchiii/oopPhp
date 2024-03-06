91939f7bd8807fd1c0d9c0a08250287e28bf3d30
nitchiii <148184975+nitchiii@users.noreply.github.com>
2024-03-06 12:30:52 +0800

    Add files via upload

---
+++ oopPhp/vid4/index.php
@@ -0,0 +1,18 @@
+<?php
+include 'person.inc.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // $pet01 = new Pet();
+    $pet01 = new Person();
+    echo $pet01->owner();
+    ?>
+</body>
+</html>
---
+++ oopPhp/vid4/person.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+class Person {
+    private $first = "Mae";
+    private $last = "Comcom";
+    private $age = "19";
+    private $occupation = "engineer";
+
+    public function owner () {
+        $a = $this->first. ?>
+        $this->last. $this->age. $this->occupation;
+        return $a;
+    }
+}
+
+class Pet {
+    // public function owner () {
+    //     $a = "MAY QUIZ MAMAYA!";
+    //     return $a;
+    
+    // }
+}
+
+
